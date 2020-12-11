4319d01761e95b46add51ce3ad7bebecc9e9ec2b
Tarik Hamilton <thamilton@masonite.com>
2020-12-10 23:34:12 -0500

    Add React component in TypeScript

--- plopfile.ts
+++ plopfile.ts
@@ -1,6 +1,10 @@
+import path from 'path'
 import { NodePlopAPI } from 'plop'
 
+const helpers = require('handlebars-helpers')()
+
 export default function (plop: NodePlopAPI) {
+  plop.setHelper('eq', helpers.eq)
   plop.setGenerator('aoc', {
     description: 'Add the next Advent of Code day files',
     prompts: [],
@@ -26,16 +30,68 @@ export default function (plop: NodePlopAPI) {
     description: 'React component',
     prompts: [
       { type: 'input', name: 'name', message: 'Name?' },
+      {
+        type: 'input',
+        name: 'path',
+        message: 'Path?',
+        default: 'src/component',
+      },
+      {
+        type: 'list',
+        name: 'css',
+        message: 'Include CSS?',
+        choices: [
+          { name: 'CSS', value: 'css' },
+          { name: 'CSS Module', value: 'module' },
+          {
+            name: 'CSS Module w/ Sass',
+            value: 'scss-module',
+          },
+          {
+            name: 'CSS Module w/ Sass + MDL',
+            value: 'scss-module--mdl',
+          },
+        ],
+      },
       { type: 'confirm', name: 'dumb', message: 'Dumb af?' },
     ],
-    actions: () => {
-      return [
+    actions: (data) => {
+      const actions = [
         {
           type: 'add',
-          path: 'src/components/{{name}}/index.js',
-          templateFile: 'templates/aoc.hbs',
+          skipIfExists: true,
+          path: path.join(
+            path.relative(__dirname, '.'),
+            '{{ path }}',
+            '/{{name}}/index.tsx'
+          ),
+          templateFile: 'templates/react/fc.ts.hbs',
+        },
+        {
+          type: 'add',
+          skipIfExists: true,
+          path: path.join(
+            path.relative(__dirname, '.'),
+            '{{ path }}',
+            '/{{name}}/index.stories.tsx'
+          ),
+          templateFile: 'templates/react/stories.ts.hbs',
         },
       ]
+
+      data?.css === 'scss-module--mdl' &&
+        actions.push({
+          type: 'add',
+          skipIfExists: true,
+          path: path.join(
+            path.relative(__dirname, '.'),
+            '{{ path }}',
+            '/{{name}}/index.module.scss'
+          ),
+          templateFile: 'templates/react/scss.hbs',
+        })
+
+      return actions
     },
   })
 }
